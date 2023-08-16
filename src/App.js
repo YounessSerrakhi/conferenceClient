d3a74b139d8f3ef3e55a1d2f20e995b6a65a2c31
Abdelhadi EL20 <abdoelbcir33@gmail.com>
2023-08-16 16:14:30 +0100

    adding list speakers code and create speaker

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import StoreSpeaker from './components/speaker/StoreSpeaker';
+import ListSpeakers from './components/speaker/ListSpeakers';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ListSpeakers/>
       <Main />
       <Footer />
     </div>

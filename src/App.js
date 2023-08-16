438ac945f09b34c69becc6ce3c01de180948ef0b
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-08-16 13:08:39 +0100

    adding speaker components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './assets/css/main.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import StoreSpeaker from './components/speaker/StoreSpeaker';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+
+      <Main />
+      <Footer />
     </div>
   );
 }

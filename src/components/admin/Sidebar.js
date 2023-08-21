c94df5e57e714c375f77449ed0bc1d8365a54d62
ABDELHADI EL BCIR <105753053+Abdelhadi-EL-bcir@users.noreply.github.com>
2023-08-21 22:01:47 +0100

    Update Sidebar.js

--- src/components/admin/Sidebar.js
+++ src/components/admin/Sidebar.js
@@ -7,30 +7,31 @@ import {
     CDBSidebarMenu,
     CDBSidebarMenuItem,
 } from 'cdbreact';
-import { NavLink,Link,useNavigate} from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../Contexts/AuthContext';
-
 
 
 const Sidebar = () => {
 
-    const {logout} = useAuth();
-    
-  const navigate = useNavigate();
-    const handleLogout =(event) => {
+    const { logout } = useAuth();
+
+    const navigate = useNavigate();
+    const handleLogout = (event) => {
         event.preventDefault();
-          logout();
-          navigate('/');
-      };
-      
+        logout();
+        navigate('/');
+    };
+
 
     return (
-        <div style={{ borderRadius:'0' , display: 'flex', height: '150vh', overflow: 'scroll initial', backgroundColor: '#343a40' }}>
+        <div style={{ borderRadius: '0', display: 'flex', height: '150vh', overflow: 'scroll initial', backgroundColor: '#343a40' }}>
             <CDBSidebar textColor="#fff" backgroundColor="#343a40">
                 <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-                    <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-                        Admin
-                    </a>
+                    <Link to="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
+                        </svg>
+                    </Link>
                 </CDBSidebarHeader>
 
                 <CDBSidebarContent className="sidebar-content">
@@ -51,7 +52,7 @@ const Sidebar = () => {
                             <CDBSidebarMenuItem icon="table">activities</CDBSidebarMenuItem>
                         </NavLink>
                         <NavLink exact to="/messages" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem icon="contact">messages</CDBSidebarMenuItem>
+                            <CDBSidebarMenuItem icon="sticky-note">messages</CDBSidebarMenuItem>
                         </NavLink>
                         <NavLink exact to="/hero404" target="_blank" activeClassName="activeClicked">
                             <CDBSidebarMenuItem icon="exclamation-circle">info</CDBSidebarMenuItem>
@@ -60,9 +61,9 @@ const Sidebar = () => {
                 </CDBSidebarContent>
 
                 <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                <Link to="#" className="nav-link" onClick={handleLogout}>
-                    Logout
-                </Link>
+                    <Link to="#" className="nav-link" onClick={handleLogout}>
+                        Logout
+                    </Link>
                     <div
                         style={{
                             padding: '20px 5px',
